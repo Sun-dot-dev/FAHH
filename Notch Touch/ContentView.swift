aed1283918f7047d8a656edbd791420cadeba53d
Suaib Sarowar <suaibsarowar95@gmail.com>
2026-03-02 18:14:26 +0530

    Initial Commit

---
+++ Notch Touch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notch Touch
+//
+//  Created by Suaib Sarowar on 02/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notch Touch/Notch_TouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notch_TouchApp.swift
+//  Notch Touch
+//
+//  Created by Suaib Sarowar on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Notch_TouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
